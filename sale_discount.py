edd99e0f71d61de1174c147634258a4cd289afb3
Albert Cervera i Areny <albert@nan-tic.com>
2024-07-06 01:15:13 +0200

    Adapt to new discount design.

--- sale_discount.py
+++ sale_discount.py
@@ -4,10 +4,10 @@ from trytond.model import fields
 class SaleLine(metaclass=PoolMeta):
     __name__ = 'sale.line'
 
-    @fields.depends('gross_unit_price')
+    @fields.depends('base_price')
     def on_change_with_margin(self, name=None):
         return super().on_change_with_margin(name)
 
-    @fields.depends('gross_unit_price')
+    @fields.depends('base_price')
     def on_change_with_margin_percent(self, name=None):
         return super().on_change_with_margin_percent(name)
